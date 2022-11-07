77c874c34c0b703a84436aa2110a920fb86397b7
Md. Samiul Islam <samiul15-6903@diu.edu.bd>
2022-11-08 01:40:52 +0600

    index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Service Review Server Running");
+});
+
+app.listen(port, () => {
+  console.log(`Service Review Server running on port ${port}`);
+});
