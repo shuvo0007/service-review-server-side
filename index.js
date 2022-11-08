cb02042e858fe995f55a72b52a1f0f0204016a3b
Md. Samiul Islam <samiul15-6903@diu.edu.bd>
2022-11-08 09:27:40 +0600

    index update

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+const category = require("./data/category.json");
 
 app.get("/", (req, res) => {
   res.send("Service Review Server Running");
+});
+
+app.get("/category", (req, res) => {
+  res.send(category);
 });
 
 app.listen(port, () => {
