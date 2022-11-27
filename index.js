d0f0e07e8dca3f826762ac4b9137a675998e4979
Md. Samiul Islam <samiul15-6903@diu.edu.bd>
2022-11-27 17:09:35 +0600

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const uri =
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1
+  serverApi: ServerApiVersion.v1,
 });
 
 async function run() {
